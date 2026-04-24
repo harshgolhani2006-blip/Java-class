089437e306783e4d02900db322c98d1bd1a706e9
harshgolhani2006-blip <harshgolhani2006@gmail.com>
2026-04-24 09:57:53 +0530

    Pattern

--- DSA/Seventh class/Tenth.java
+++ DSA/Seventh class/Tenth.java
@@ -160,23 +160,77 @@ import java.util.Scanner;
 // }
 // }
 
-public class Nineth{
-    public static void main(String[] args) {
-     Scanner sc = new Scanner(System.in);
-         System.out.print("Enter the value of n :- ");
-        int n = sc.nextInt();
-        for(int i=1;i<=n;i++){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-        for(int i=n-1;i>=1;i--){
-            for(int j=1;j<=i;j++){
-                System.out.print("*");
-            }
-            System.out.println();
-        }
-}
-}
+// public class Nineth{
+//     public static void main(String[] args) {
+//      Scanner sc = new Scanner(System.in);
+//          System.out.print("Enter the value of n :- ");
+//         int n = sc.nextInt();
+//         for(int i=1;i<=n;i++){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//         for(int i=n-1;i>=1;i--){
+//             for(int j=1;j<=i;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+// }
+// }
 
+public class Tenth {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter the value of n : ");
+        int n = sc.nextInt();
+
+        // Upper Part of Heart
+        for (int i = n / 2; i <= n; i += 2) {
+
+            // Left spaces
+            for (int j = 1; j < n - i; j += 2) {
+                System.out.print(" ");
+            }
+
+            // Left stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            // Middle spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+
+            // Right stars
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        // Lower Part of Heart
+        for (int i = n; i >= 1; i--) {
+
+            // Starting spaces
+            for (int j = i; j < n; j++) {
+                System.out.print(" ");
+            }
+
+            // Stars
+            for (int j = 1; j <= (i * 2) - 1; j++) {
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+
+        sc.close();
+    }
+}
